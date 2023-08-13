8e0b22a4b7ff3ecff0b8e801d1c5b1652de2afe8
LT_Name <572413378@qq.com>
2023-08-13 08:12:36 +0800

    Stuff

--- src/main/java/cn/nukkit/inventory/SmithingInventory.java
+++ src/main/java/cn/nukkit/inventory/SmithingInventory.java
@@ -111,7 +111,7 @@ public class SmithingInventory extends FakeBlockUIComponent {
 
         this.clear(EQUIPMENT);
         this.clear(INGREDIENT);
-        this.getHolder().getInventory().clear(50);
+        this.getHolder().getInventory().clear(CREATED_ITEM_OUTPUT_UI_SLOT);
     }
 
     public Item getCurrentResult() {
