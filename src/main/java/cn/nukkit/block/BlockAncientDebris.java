b55906dbfb698dd8c10111b2e3a242500a64b7a8
LT_Name <572413378@qq.com>
2023-08-22 20:03:27 +0800

    Stuff

--- src/main/java/cn/nukkit/block/BlockAncientDebris.java
+++ src/main/java/cn/nukkit/block/BlockAncientDebris.java
@@ -5,46 +5,46 @@ import cn.nukkit.item.ItemBlock;
 import cn.nukkit.item.ItemTool;
 
 public class BlockAncientDebris extends BlockSolid {
-	@Override
-	public String getName() {
-		return "Ancient Debris";
-	}
+    @Override
+    public String getName() {
+        return "Ancient Debris";
+    }
 
-	@Override
-	public int getId() {
-		return ANCIENT_DEBRIS;
-	}
+    @Override
+    public int getId() {
+        return ANCIENT_DEBRIS;
+    }
 
-	@Override
-	public double getHardness() {
-		return 30;
-	}
+    @Override
+    public double getHardness() {
+        return 30;
+    }
 
-	@Override
-	public double getResistance() {
-		return 1200;
-	}
+    @Override
+    public double getResistance() {
+        return 1200;
+    }
 
-	@Override
-	public int getToolType() {
-		return ItemTool.TYPE_PICKAXE;
-	}
+    @Override
+    public int getToolType() {
+        return ItemTool.TYPE_PICKAXE;
+    }
 
-	@Override
-	public Item[] getDrops(final Item item) {
-		if (item.isPickaxe()) {
-			return new Item[]{new ItemBlock(this, 0, 1)};
-		}
-		return Item.EMPTY_ARRAY;
-	}
+    @Override
+    public Item[] getDrops(final Item item) {
+        if (item.isPickaxe()) {
+            return new Item[]{new ItemBlock(this, 0, 1)};
+        }
+        return Item.EMPTY_ARRAY;
+    }
 
-	@Override
-	public boolean canHarvestWithHand() {
-		return false;
-	}
+    @Override
+    public boolean canHarvestWithHand() {
+        return false;
+    }
 
-	@Override
-	public int getToolTier() {
-		return ItemTool.TIER_DIAMOND;
-	}
+    @Override
+    public int getToolTier() {
+        return ItemTool.TIER_DIAMOND;
+    }
 }
