98a20ed1ec6e5a22829ec94901b3e777cdf501d0
BlusterySasha <blusterysasha@vk.com>
2023-08-24 22:34:12 +0400

    revert BlockTrapdoorIron

--- src/main/java/cn/nukkit/block/BlockTrapdoorIron.java
+++ src/main/java/cn/nukkit/block/BlockTrapdoorIron.java
@@ -5,51 +5,51 @@ import cn.nukkit.item.Item;
 import cn.nukkit.item.ItemTool;
 
 public class BlockTrapdoorIron extends BlockTrapdoor {
-	public BlockTrapdoorIron() {
-		super(0);
-	}
+    public BlockTrapdoorIron() {
+        super(0);
+    }
 
-	public BlockTrapdoorIron(final int meta) {
-		super(meta);
-	}
+    public BlockTrapdoorIron(int meta) {
+        super(meta);
+    }
 
-	@Override
-	public int getId() {
-		return IRON_TRAPDOOR;
-	}
+    @Override
+    public int getId() {
+        return IRON_TRAPDOOR;
+    }
 
-	@Override
-	public String getName() {
-		return "Iron Trapdoor";
-	}
+    @Override
+    public String getName() {
+        return "Iron Trapdoor";
+    }
 
-	@Override
-	public double getHardness() {
-		return 5;
-	}
+    @Override
+    public double getHardness() {
+        return 5;
+    }
 
-	@Override
-	public double getResistance() {
-		return 25;
-	}
+    @Override
+    public double getResistance() {
+        return 25;
+    }
 
-	@Override
-	public int getToolType() {
-		return ItemTool.TYPE_PICKAXE;
-	}
+    @Override
+    public int getToolType() {
+        return ItemTool.TYPE_PICKAXE;
+    }
 
-	@Override
-	public int getToolTier() {
-		return ItemTool.TIER_WOODEN;
-	}
+    @Override
+    public int getToolTier() {
+        return ItemTool.TIER_WOODEN;
+    }
 
-	@Override
-	public boolean onActivate(final Item item, final Player player) {
-		return false;
-	}
+    @Override
+    public boolean onActivate(final Item item, final Player player) {
+        return false;
+    }
 
-	@Override
-	public boolean canHarvestWithHand() {
-		return false;
-	}
+    @Override
+    public boolean canHarvestWithHand() {
+        return false;
+    }
 }
