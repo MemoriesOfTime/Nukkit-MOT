023b64c9a31650657371dcf4b47ceaeb5a209213
LT_Name <572413378@qq.com>
2023-08-24 21:38:14 +0800

    添加BlockRootsCrimson和BlockRootsWarped

---
+++ src/main/java/cn/nukkit/block/BlockRoots.java
@@ -0,0 +1,40 @@
+package cn.nukkit.block;
+
+import cn.nukkit.Player;
+import cn.nukkit.item.Item;
+import cn.nukkit.level.Level;
+import cn.nukkit.math.BlockFace;
+import org.jetbrains.annotations.NotNull;
+
+public abstract class BlockRoots extends BlockFlowable {
+
+    protected BlockRoots() {
+        super(0);
+    }
+
+    @Override
+    public int onUpdate(int type) {
+        if (type == Level.BLOCK_UPDATE_NORMAL && !isSupportValid()) {
+            level.useBreakOn(this);
+            return type;
+        }
+        return 0;
+    }
+
+    @Override
+    public boolean place(@NotNull Item item, @NotNull Block block, @NotNull Block target, @NotNull BlockFace face, double fx, double fy, double fz, Player player) {
+        return isSupportValid() && super.place(item, block, target, face, fx, fy, fz, player);
+    }
+
+    protected boolean isSupportValid() {
+        return switch (down().getId()) {
+            case WARPED_NYLIUM, CRIMSON_NYLIUM, GRASS, PODZOL, DIRT, SOUL_SOIL -> true;
+            default -> false;
+        };
+    }
+
+    @Override
+    public int getBurnChance() {
+        return 5;
+    }
+}
---
+++ src/main/java/cn/nukkit/block/BlockRootsWarped.java
@@ -0,0 +1,19 @@
+package cn.nukkit.block;
+
+public class BlockRootsWarped extends BlockRoots {
+
+    public BlockRootsWarped() {
+        // Does nothing
+    }
+
+    @Override
+    public int getId() {
+        return WARPED_ROOTS;
+    }
+
+    @Override
+    public String getName() {
+        return "Warped Roots";
+    }
+
+}
