673850c1a275eef7de7a3d137a2544582e565069
glorydark <1083215364@qq.com>
2023-09-07 22:06:45 +0800

    Add missing BlockOreCopper

---
+++ src/main/java/cn/nukkit/block/BlockOreCopper.java
@@ -0,0 +1,82 @@
+package cn.nukkit.block;
+
+import cn.nukkit.item.Item;
+import cn.nukkit.item.ItemRawCopper;
+import cn.nukkit.item.ItemTool;
+import cn.nukkit.item.enchantment.Enchantment;
+import cn.nukkit.utils.Utils;
+
+/**
+ * @author MagicDroidX
+ * Nukkit Project
+ */
+public class BlockOreCopper extends BlockSolid {
+
+    @Override
+    public int getId() {
+        return COPPER_ORE;
+    }
+
+    @Override
+    public double getHardness() {
+        return 3;
+    }
+
+    @Override
+    public double getResistance() {
+        return 15;
+    }
+
+    @Override
+    public int getToolType() {
+        return ItemTool.TYPE_PICKAXE;
+    }
+
+    @Override
+    public int getToolTier() {
+        return ItemTool.TIER_WOODEN;
+    }
+
+    @Override
+    public String getName() {
+        return "Copper Ore";
+    }
+
+    @Override
+    public Item[] getDrops(Item item) {
+        if (item.isPickaxe() && item.getTier() >= this.getToolTier()) {
+            if (item.hasEnchantment(Enchantment.ID_SILK_TOUCH)) {
+                return new Item[]{this.toItem()};
+            }
+            int count = 1;
+            Enchantment fortune = item.getEnchantment(Enchantment.ID_FORTUNE_DIGGING);
+            if (fortune != null && fortune.getLevel() >= 1) {
+                int i = Utils.random.nextInt(fortune.getLevel() + 2) - 1;
+
+                if (i < 0) {
+                    i = 0;
+                }
+
+                count = i + 1;
+            }
+
+            ItemRawCopper rawCopper = new ItemRawCopper();
+            rawCopper.setCount(count);
+            return new Item[]{
+                    rawCopper
+            };
+        } else {
+            return Item.EMPTY_ARRAY;
+        }
+    }
+
+    @Override
+    public boolean canHarvestWithHand() {
+        return false;
+    }
+    
+    @Override
+    public boolean canSilkTouch() {
+        return true;
+    }
+}
