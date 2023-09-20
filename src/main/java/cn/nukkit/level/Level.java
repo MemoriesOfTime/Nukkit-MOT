7edd3d1153e75edbf183efc8cb344462f792e76f
LT_Name <572413378@qq.com>
2023-09-20 21:39:22 +0800

    NukkitX PR 2149 - Fix entity movement on snow layers

--- src/main/java/cn/nukkit/level/Level.java
+++ src/main/java/cn/nukkit/level/Level.java
@@ -4077,12 +4077,12 @@ public class Level implements ChunkManager, Metadatable {
     public void addEntityMovement(Entity entity, double x, double y, double z, double yaw, double pitch, double headYaw) {
         MoveEntityAbsolutePacket pk = new MoveEntityAbsolutePacket();
         pk.eid = entity.getId();
-        pk.x = (float) x;
-        pk.y = (float) y;
-        pk.z = (float) z;
-        pk.yaw = (float) yaw;
-        pk.headYaw = (float) headYaw;
-        pk.pitch = (float) pitch;
+        pk.x = x;
+        pk.y = y;
+        pk.z = z;
+        pk.yaw = yaw;
+        pk.headYaw = headYaw;
+        pk.pitch = pitch;
         pk.onGround = entity.onGround;
 
         if (this.server.isLowProfileServer()) {
