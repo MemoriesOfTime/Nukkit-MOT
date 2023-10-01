2e9f6f91a98e053422203a92541141f0c4cb745b
LT_Name <572413378@qq.com>
2023-10-01 20:00:57 +0800

    Ingot Copper is only available in version 1.17.0 and above

--- src/main/java/cn/nukkit/item/ItemIngotCopper.java
+++ src/main/java/cn/nukkit/item/ItemIngotCopper.java
@@ -1,5 +1,7 @@
 package cn.nukkit.item;
 
+import cn.nukkit.network.protocol.ProtocolInfo;
+
 /**
  * @author MagicDroidX
  * Nukkit Project
@@ -8,5 +10,10 @@ public class ItemIngotCopper extends StringItemBase {
 
     public ItemIngotCopper() {
         super("minecraft:copper_ingot", "Copper Ingot");
+    }
+
+    @Override
+    public boolean isSupportedOn(int protocolId) {
+        return protocolId >= ProtocolInfo.v1_17_0;
     }
 }
