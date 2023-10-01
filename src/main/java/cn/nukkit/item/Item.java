d46db91bd1374e58b4277990d07e4defb643dccd
LT_Name <572413378@qq.com>
2023-10-01 21:36:49 +0800

    Update Recipes

--- src/main/java/cn/nukkit/item/Item.java
+++ src/main/java/cn/nukkit/item/Item.java
@@ -1631,7 +1631,7 @@ public class Item implements Cloneable, BlockID, ItemID, ProtocolInfo {
 
     public final boolean equals(Item item, boolean checkDamage, boolean checkCompound) {
         if (this.id == STRING_IDENTIFIED_ITEM && item.id == STRING_IDENTIFIED_ITEM) {
-            if (!this.getNamespaceId().equals(item.getNamespaceId())) {
+            if (!this.getNamespaceId(ProtocolInfo.CURRENT_PROTOCOL).equals(item.getNamespaceId(ProtocolInfo.CURRENT_PROTOCOL))) {
                 return false;
             }
         } else if (this.id != item.id) {
