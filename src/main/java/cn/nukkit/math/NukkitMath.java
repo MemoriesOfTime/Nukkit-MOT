3137de779b33bacb8cc26a6ca785a49e80bc3c95
LT_Name <572413378@qq.com>
2023-10-04 09:05:06 +0800

    functional

--- src/main/java/cn/nukkit/math/NukkitMath.java
+++ src/main/java/cn/nukkit/math/NukkitMath.java
@@ -1,5 +1,7 @@
 package cn.nukkit.math;
 
+import java.math.BigInteger;
+
 /**
  * @author MagicDroidX
  * Nukkit Project
@@ -63,5 +65,67 @@ public class NukkitMath {
         diffZ = Math.abs(diffZ);
 
         return Math.max(diffX, diffZ);
+    }
+
+    public static int bitLength(byte data) {
+        if (data < 0) {
+            return 32;
+        }
+
+        if (data == 0) {
+            return 1;
+        }
+
+        int bits = 0;
+        while (data != 0) {
+            data >>>= 1;
+            bits++;
+        }
+
+        return bits;
+    }
+
+    public static int bitLength(int data) {
+        if (data < 0) {
+            return 32;
+        }
+
+        if (data == 0) {
+            return 1;
+        }
+
+        int bits = 0;
+        while (data != 0) {
+            data >>>= 1;
+            bits++;
+        }
+
+        return bits;
+    }
+
+    public static int bitLength(long data) {
+        if (data < 0) {
+            return 64;
+        }
+
+        if (data == 0) {
+            return 1;
+        }
+
+        int bits = 0;
+        while (data != 0) {
+            data >>>= 1;
+            bits++;
+        }
+
+        return bits;
+    }
+
+    public static int bitLength(BigInteger data) {
+        if (data.compareTo(BigInteger.ZERO) < 0) {
+            throw new UnsupportedOperationException("Negative BigIntegers are not supported (nearly infinite bits)");
+        }
+
+        return data.bitLength();
     }
 }
