04594c869e2dc64adc420d9ec7375004e958bafb
LT_Name <572413378@qq.com>
2023-10-14 19:43:16 +0800

    Fix CustomEntity conflict issues

--- src/main/java/cn/nukkit/entity/custom/CustomEntity.java
+++ src/main/java/cn/nukkit/entity/custom/CustomEntity.java
@@ -4,10 +4,6 @@ public interface CustomEntity {
 
     EntityDefinition getEntityDefinition();
 
-    default String getIdentifier() {
-        return this.getEntityDefinition().getIdentifier();
-    }
-
     default int getNetworkId() {
         return this.getEntityDefinition().getRuntimeId();
     }
