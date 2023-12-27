bd24b8e6531a301d5ae687a5115d0c2cdc32671a
LT_Name <45508179+lt-name@users.noreply.github.com>
2023-12-27 21:52:31 +0800

    Update setDamage (#132)

--- src/main/java/cn/nukkit/block/BlockMeta.java
+++ src/main/java/cn/nukkit/block/BlockMeta.java
@@ -18,8 +18,28 @@ public abstract class BlockMeta extends Block {
         return this.meta;
     }
 
+    public int getDamage(int flagBit) {
+        return this.getDamage() & flagBit;
+    }
+
     @Override
     public void setDamage(int meta) {
         this.meta = meta;
+    }
+
+    public void setDamage(int flagBit, int data) {
+        this.setDamage(this.getDamage() & ~flagBit | data << getFlagBitLength(flagBit) & flagBit);
+    }
+
+    private int getFlagBitLength(int flagBit) {
+        int count = 0;
+        while(flagBit != 1) {
+            if (((flagBit & 1) == 1)) {
+                break;
+            }
+            count++;
+            flagBit >>>= 1;
+        }
+        return count;
     }
 }
