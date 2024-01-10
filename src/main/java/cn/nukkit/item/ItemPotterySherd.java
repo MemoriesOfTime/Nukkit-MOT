a434abc3e2181630bf1d4bbf6d1e546c2288694d
LT_Name <572413378@qq.com>
2024-01-10 18:27:01 +0800

    ItemPotterySherd to abstract class and add version limit

--- src/main/java/cn/nukkit/item/ItemPotterySherd.java
+++ src/main/java/cn/nukkit/item/ItemPotterySherd.java
@@ -1,5 +1,6 @@
 package cn.nukkit.item;
 
+import cn.nukkit.network.protocol.ProtocolInfo;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
@@ -7,10 +8,14 @@ import org.jetbrains.annotations.Nullable;
  * @author glorydark
  * @date {2024/1/10} {12:08}
  */
-public class ItemPotterySherd extends StringItemBase {
+public abstract class ItemPotterySherd extends StringItemBase {
 
     public ItemPotterySherd(@NotNull String id, @Nullable String name) {
         super(id, name);
     }
 
+    @Override
+    public boolean isSupportedOn(int protocolId) {
+        return protocolId >= ProtocolInfo.v1_20_0_23;
+    }
 }
