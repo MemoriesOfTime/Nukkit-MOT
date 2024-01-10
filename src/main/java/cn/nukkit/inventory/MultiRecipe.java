a37c67cfc20ee18c25a22962d0049fbf25213f49
glorydark <1083215364@qq.com>
2024-01-10 17:45:58 +0800

    Add TYPE_DECORATED_POT_RECIPE

--- src/main/java/cn/nukkit/inventory/MultiRecipe.java
+++ src/main/java/cn/nukkit/inventory/MultiRecipe.java
@@ -18,6 +18,8 @@ public class MultiRecipe implements Recipe {
     public static final String TYPE_MAP_CLONING = "85939755-BA10-4D9D-A4CC-EFB7A8E943C4";
     public static final String TYPE_MAP_UPGRADING = "AECD2294-4B94-434B-8667-4499BB2C9327";
     public static final String TYPE_BOOK_CLONING = "D1CA6B84-338E-4F2F-9C6B-76CC8B4BD98D";
+    public static final String TYPE_DECORATED_POT_RECIPE = "685a742a-c42e-4a4e-88ea-5eb83fc98e5b";
+
     public static final String TYPE_BANNER_DUPLICATE = "B5C5D105-75A2-4076-AF2B-923EA2BF4BF0";
     public static final String TYPE_BANNER_ADD_PATTERN = "D81AAEAF-E172-4440-9225-868DF030D27B";
     public static final String TYPE_FIREWORKS = "00000000-0000-0000-0000-000000000002"; // already done by recipe json files
