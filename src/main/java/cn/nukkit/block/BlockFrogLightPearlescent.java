8001774e037141d60d94add046bd980dbdc3c476
glorydark <1083215364@qq.com>
2024-01-23 01:55:33 +0800

    Stuff
    * Update runtime block states from 527 to 630(https://minecraft.fandom.com/wiki/Bedrock_Edition_1.19.0)
    * Add runtime_item_states_630.json
    * Add BlockFrogLight(Inspired by BlockStem.java)

---
+++ src/main/java/cn/nukkit/block/BlockFrogLightPearlescent.java
@@ -0,0 +1,20 @@
+package cn.nukkit.block;
+
+import cn.nukkit.item.Item;
+import cn.nukkit.item.ItemBlock;
+
+/**
+ * @author glorydark
+ */
+public class BlockFrogLightPearlescent extends BlockFrogLight {
+
+    @Override
+    public String getName() {
+        return "Pearlescent FrogLight";
+    }
+
+    @Override
+    public int getId() {
+        return PEARLESCENT_FROGLIGHT;
+    }
+}
