b8b41c4d40635a5150b2133ac1941bc11c142f00
LT_Name <572413378@qq.com>
2024-02-06 20:10:52 +0800

    Add SetPlayerInventoryOptionsPacket

---
+++ src/main/java/cn/nukkit/network/protocol/SetPlayerInventoryOptionsPacket.java
@@ -0,0 +1,45 @@
+package cn.nukkit.network.protocol;
+
+import cn.nukkit.network.protocol.types.inventory.InventoryLayout;
+import cn.nukkit.network.protocol.types.inventory.InventoryTabLeft;
+import cn.nukkit.network.protocol.types.inventory.InventoryTabRight;
+
+public class SetPlayerInventoryOptionsPacket extends DataPacket {
+
+    public static final int NETWORK_ID = ProtocolInfo.SET_PLAYER_INVENTORY_OPTIONS_PACKET;
+
+    public InventoryTabLeft leftTab;
+    public InventoryTabRight rightTab;
+    public boolean filtering;
+    public InventoryLayout layout;
+    public InventoryLayout craftingLayout;
+
+    @Override
+    public int packetId() {
+        return NETWORK_ID;
+    }
+
+    @Override
+    public byte pid() {
+        return ProtocolInfo.__INTERNAL__SET_PLAYER_INVENTORY_OPTIONS_PACKET;
+    }
+
+    @Override
+    public void decode() {
+        this.leftTab = InventoryTabLeft.VALUES[this.getVarInt()];
+        this.rightTab = InventoryTabRight.VALUES[this.getVarInt()];
+        this.filtering = this.getBoolean();
+        this.layout = InventoryLayout.VALUES[this.getVarInt()];
+        this.craftingLayout = InventoryLayout.VALUES[this.getVarInt()];
+    }
+
+    @Override
+    public void encode() {
+        this.reset();
+        this.putVarInt(this.leftTab.ordinal());
+        this.putVarInt(this.rightTab.ordinal());
+        this.putBoolean(this.filtering);
+        this.putVarInt(this.layout.ordinal());
+        this.putVarInt(this.craftingLayout.ordinal());
+    }
+}
