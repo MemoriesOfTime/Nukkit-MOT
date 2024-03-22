a0e9f3b344502e5692b98130d3d8ea338f79d693
labarjni <labarjni@mail.ru>
2024-03-23 02:11:57 +0300

    Fix PlayerDuplicateLoginEvent

--- src/main/java/cn/nukkit/event/player/PlayerDuplicateLoginEvent.java
+++ src/main/java/cn/nukkit/event/player/PlayerDuplicateLoginEvent.java
@@ -3,17 +3,23 @@ package cn.nukkit.event.player;
 import cn.nukkit.Player;
 import cn.nukkit.event.Cancellable;
 import cn.nukkit.event.HandlerList;
-import lombok.Getter;
 
-@Getter
 public class PlayerDuplicateLoginEvent extends PlayerEvent implements Cancellable {
 
     private static final HandlerList handlers = new HandlerList();
+
+    public static HandlerList getHandlers() {
+        return handlers;
+    }
 
     private final Player originalPlayer;
 
     public PlayerDuplicateLoginEvent(Player player, Player originalPlayer) {
         this.player = player;
         this.originalPlayer = originalPlayer;
+    }
+
+    public Player getOriginalPlayer() {
+        return originalPlayer;
     }
 }
