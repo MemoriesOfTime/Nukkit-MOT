634fbcf0f90c5f0df22b0ee3988c07e948f832bd
Glorydark <61799945+glorydark@users.noreply.github.com>
2024-04-14 07:32:26 +0800

    Add missing amethyst block (#239)
    * Add missing amethyst block
    
    * Update blockstates 440-527

---
+++ src/main/java/cn/nukkit/block/BlockAmethyst.java
@@ -0,0 +1,45 @@
+package cn.nukkit.block;
+
+import cn.nukkit.item.ItemTool;
+
+public class BlockAmethyst extends BlockSolid {
+
+    public BlockAmethyst() {
+        super();
+    }
+
+    @Override
+    public String getName() {
+        return "Block of Amethyst";
+    }
+
+    @Override
+    public int getId() {
+        return AMETHYST_BLOCK;
+    }
+
+    @Override
+    public double getHardness() {
+        return 1.5;
+    }
+
+    @Override
+    public double getResistance() {
+        return 1.5;
+    }
+
+    @Override
+    public int getToolType() {
+        return ItemTool.TYPE_PICKAXE;
+    }
+
+    @Override
+    public int getToolTier() {
+        return ItemTool.TIER_IRON;
+    }
+
+    @Override
+    public boolean canHarvestWithHand() {
+        return false;
+    }
+}
