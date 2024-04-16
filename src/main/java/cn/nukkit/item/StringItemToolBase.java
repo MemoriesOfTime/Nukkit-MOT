2bbce64685d2dd9b4fe613b34750538469ed276f
glorydark <1083215364@qq.com>
2024-04-16 18:50:50 +0800

    Changes in StringItemToolBase

--- src/main/java/cn/nukkit/item/StringItemToolBase.java
+++ src/main/java/cn/nukkit/item/StringItemToolBase.java
@@ -1,22 +1,23 @@
 package cn.nukkit.item;
 
-import cn.nukkit.block.Block;
-import cn.nukkit.entity.Entity;
-import cn.nukkit.item.enchantment.Enchantment;
-import cn.nukkit.nbt.tag.ByteTag;
-import cn.nukkit.nbt.tag.CompoundTag;
-import cn.nukkit.nbt.tag.Tag;
-import cn.nukkit.utils.Utils;
+import cn.nukkit.network.protocol.ProtocolInfo;
+import com.google.common.base.Preconditions;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 /**
  * @author glorydark
  */
-public class StringItemToolBase extends StringItemBase implements ItemDurable {
+public class StringItemToolBase extends ItemTool implements ItemDurable, StringItem {
+
+    private final String id;
 
     public StringItemToolBase(@NotNull String id, @Nullable String name) {
-        super(id, name);
+        super(STRING_IDENTIFIED_ITEM, 0, 1, StringItem.notEmpty(name));
+        Preconditions.checkNotNull(id, "id can't be null");
+        Preconditions.checkArgument(id.contains(":"), "The ID must be a namespaced ID, like minecraft:stone");
+        this.id = id;
+        clearNamedTag();
     }
 
     @Override
@@ -24,99 +25,14 @@ public class StringItemToolBase extends StringItemBase implements ItemDurable {
         return true;
     }
 
-    @Override
-    public int getMaxStackSize() {
-        return 1;
-    }
-
-    @Override
-    public boolean useOn(Block block) {
-        if (this.isUnbreakable() || isDurable() || noDamageOnBreak()) {
-            return true;
-        }
-
-        if (block.getToolType() == ItemTool.TYPE_PICKAXE && this.isPickaxe() ||
-                block.getToolType() == ItemTool.TYPE_SHOVEL && this.isShovel() ||
-                block.getToolType() == ItemTool.TYPE_AXE && this.isAxe() ||
-                block.getToolType() == ItemTool.TYPE_HOE && this.isHoe() ||
-                block.getToolType() == ItemTool.TYPE_SWORD && this.isSword() ||
-                block.getToolType() == ItemTool.TYPE_SHEARS && this.isShears()
-        ) {
-            this.meta++;
-        } else if (!this.isShears() && block.calculateBreakTime(this) > 0) {
-            this.meta += 2;
-        } else if (this.isSword() && block.getHardness() > 0.0) {
-            this.meta += 2;
-        } else if (this.isHoe()) {
-            if (block.getId() == GRASS || block.getId() == DIRT) {
-                this.meta++;
-            }
-        } else {
-            this.meta++;
-        }
-        return true;
-    }
-
-    @Override
-    public boolean useOn(Entity entity) {
-        if (this.isUnbreakable() || isDurable() || noDamageOnAttack()) {
-            return true;
-        }
-
-        if ((entity != null) && !this.isSword()) {
-            this.meta += 2;
-        } else {
-            this.meta++;
-        }
-
-        return true;
-    }
-
-    private boolean isDurable() {
-        if (!hasEnchantments()) {
-            return false;
-        }
-
-        Enchantment durability = getEnchantment(Enchantment.ID_DURABILITY);
-        return durability != null && durability.getLevel() > 0 && (100 / (durability.getLevel() + 1)) <= Utils.random.nextInt(100);
-    }
-
-    @Override
-    public boolean isUnbreakable() {
-        Tag tag = this.getNamedTagEntry("Unbreakable");
-        return tag instanceof ByteTag && ((ByteTag) tag).data > 0;
-    }
-
-    public void setUnbreakable(boolean unbreakable) {
-        var tag = this.getNamedTag();
-        if (tag == null) {
-            tag = new CompoundTag();
-        }
-        tag.putByte("Unbreakable", (byte) (unbreakable ? 1 : 0));
-        this.setNamedTag(tag);
-    }
-
-    @Override
-    public int getEnchantAbility() {
-        return switch (this.getTier()) {
-            case ItemTool.TIER_STONE -> 5;
-            case ItemTool.TIER_WOODEN -> 15;
-            case ItemTool.TIER_DIAMOND -> 10;
-            case ItemTool.TIER_GOLD -> 22;
-            case ItemTool.TIER_IRON -> 14;
-            //TODO
-            case ItemTool.TIER_NETHERITE -> 10;
-            default -> 0;
-        };
-    }
-
     /**
      * No damage to item when it's used to attack entities
      *
      * @return whether the item should take damage when used to attack entities
      */
+    @Override
     public boolean noDamageOnAttack() {
-        return false;
+        return true;
     }
 
     /**
@@ -124,7 +40,33 @@ public class StringItemToolBase extends StringItemBase implements ItemDurable {
      *
      * @return whether the item should take damage when used to break blocks
      */
+    @Override
     public boolean noDamageOnBreak() {
-        return false;
+        return true;
+    }
+
+    @Override
+    public String getNamespaceId() {
+        return this.id;
+    }
+
+    @Override
+    public String getNamespaceId(int protocolId) {
+        return this.getNamespaceId();
+    }
+
+    @Override
+    public final int getId() {
+        return StringItem.super.getId();
+    }
+
+    @Override
+    public StringItemBase clone() {
+        return (StringItemBase) super.clone();
+    }
+
+    @Override
+    public boolean isSupportedOn(int protocolId) {
+        return protocolId >= ProtocolInfo.v1_20_0;
     }
 }
