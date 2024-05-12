198aabef996af34082b98e266db759b3ebc1fa89
LT_Name <572413378@qq.com>
2024-05-12 21:27:54 +0800

    Stuff

--- src/main/java/cn/nukkit/entity/projectile/EntityThrownTrident.java
+++ src/main/java/cn/nukkit/entity/projectile/EntityThrownTrident.java
@@ -16,7 +16,6 @@ import cn.nukkit.level.GameRule;
 import cn.nukkit.level.MovingObjectPosition;
 import cn.nukkit.level.Sound;
 import cn.nukkit.level.format.FullChunk;
-import cn.nukkit.math.BVector3;
 import cn.nukkit.math.BlockVector3;
 import cn.nukkit.math.Vector3;
 import cn.nukkit.nbt.NBTIO;
@@ -226,10 +225,7 @@ public class EntityThrownTrident extends EntitySlenderProjectile {
                 Entity shooter = this.shootingEntity;
                 double force = 0.05d * (double) loyaltyLevel;
                 Vector3 vector3 = new Vector3(shooter.x - this.x, shooter.y + shooter.getEyeHeight() - this.y, shooter.z - this.z);
-                BVector3 bVector = BVector3.fromPos(vector3);
-                vector3 = bVector.addToPos();
                 this.setPosition(new Vector3(this.x + vector3.x * force, this.y + vector3.y * force, this.z + vector3.z * force));
-                this.setRotation(bVector.getYaw(), bVector.getPitch());
                 this.setMotion(vector3.multiply(force));
                 hasUpdate = true;
             } else {
