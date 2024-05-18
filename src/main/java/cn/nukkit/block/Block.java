394164cfda578ff74e7cf2da0cbba7f608a27664
QwQ-dev <peterramos1456@gmail.com>
2024-05-18 08:58:11 +0800

    Added CRIMSON_BUTTON, WARPED_BUTTON, CRIMSON_PRESSURE_PLATE, WARPED_PRESSURE_PLATE.

--- src/main/java/cn/nukkit/block/Block.java
+++ src/main/java/cn/nukkit/block/Block.java
@@ -477,6 +477,10 @@ public abstract class Block extends Position implements Metadatable, Cloneable, 
             list[WARPED_FENCE] = BlockFenceWarped.class; //512
             list[CRIMSON_FENCE_GATE] = BlockFenceGateCrimson.class; //513
             list[WARPED_FENCE_GATE] = BlockFenceGateWarped.class; //514
+            list[CRIMSON_BUTTON] = BlockButtonCrimson.class; //515
+            list[WARPED_BUTTON] = BlockButtonWarped.class; //516
+            list[CRIMSON_PRESSURE_PLATE] = BlockPressurePlateCrimson.class; //517
+            list[WARPED_PRESSURE_PLATE] = BlockPressurePlateWarped.class; //518
 
             list[SOUL_TORCH] = BlockSoulTorch.class; //523
             list[SOUL_LANTERN] = BlockSoulLantern.class; //524
