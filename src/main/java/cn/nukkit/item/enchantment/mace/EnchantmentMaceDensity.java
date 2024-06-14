85d8774a8dce9fc12be7d82552bff64bf8029ea7
Glorydark <1083215364@qq.com>
2024-06-14 12:43:03 +0800

    Add new effects and enchantments

---
+++ src/main/java/cn/nukkit/item/enchantment/mace/EnchantmentMaceDensity.java
@@ -0,0 +1,41 @@
+package cn.nukkit.item.enchantment.mace;
+
+import cn.nukkit.entity.Entity;
+import cn.nukkit.item.enchantment.Enchantment;
+import cn.nukkit.item.enchantment.damage.EnchantmentDamage;
+
+public class EnchantmentMaceDensity extends EnchantmentMace {
+
+    public EnchantmentMaceDensity() {
+        super(Enchantment.ID_DENSITY, "heavy_weapon.density", Rarity.UNCOMMON);
+    }
+
+    @Override
+    public int getMaxLevel() {
+        return 5;
+    }
+
+    @Override
+    public int getMinEnchantAbility(int level) {
+        return 8 * level - 3;
+    }
+
+    @Override
+    public int getMaxEnchantAbility(int level) {
+        return 8 * level + 17;
+    }
+
+    @Override
+    public double getDamageBonus(Entity entity, Entity attacker) {
+        float fallDistance = attacker.fallDistance;
+        if (fallDistance <= 0) {
+            return 0;
+        }
+        return getLevel() * 0.5f * fallDistance;
+    }
+
+    @Override
+    public boolean checkCompatibility(Enchantment enchantment) {
+        return super.checkCompatibility(enchantment) && enchantment.id != Enchantment.ID_DENSITY && !(enchantment instanceof EnchantmentDamage);
+    }
+}
