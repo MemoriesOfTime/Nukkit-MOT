2a2665af2821177736f670ed41ff956c7740ebc1
Glorydark <1083215364@qq.com>
2024-06-20 15:58:43 +0800

    Add missing canEnchant method

--- src/main/java/cn/nukkit/item/enchantment/mace/EnchantmentMace.java
+++ src/main/java/cn/nukkit/item/enchantment/mace/EnchantmentMace.java
@@ -1,5 +1,6 @@
 package cn.nukkit.item.enchantment.mace;
 
+import cn.nukkit.item.Item;
 import cn.nukkit.item.enchantment.Enchantment;
 import cn.nukkit.item.enchantment.EnchantmentType;
 
@@ -7,5 +8,10 @@ public abstract class EnchantmentMace extends Enchantment {
 
     protected EnchantmentMace(int id, String identifier, Rarity rarity) {
         super(id, identifier, rarity, EnchantmentType.MACE);
+    }
+
+    @Override
+    public boolean canEnchant(Item item) {
+        return item.getNamespaceId().equals("minecraft:mace");
     }
 }
