13132c72fcae75905f2ceea9d2c5ea5a5484731f
Glorydark <1083215364@qq.com>
2024-06-22 00:13:02 +0800

    Add random durability functionality

--- src/main/java/cn/nukkit/item/randomitem/EnchantmentItemSelector.java
+++ src/main/java/cn/nukkit/item/randomitem/EnchantmentItemSelector.java
@@ -13,38 +13,58 @@ import java.util.concurrent.ThreadLocalRandom;
  * @author LT_Name
  */
 public class EnchantmentItemSelector extends ConstantItemSelector {
+
     public EnchantmentItemSelector(int id, Selector parent) {
         this(id, 0, parent);
+    }
+
+    public EnchantmentItemSelector(int id, Selector parent, boolean randomDurability) {
+        this(id, 0, parent, randomDurability);
     }
 
     public EnchantmentItemSelector(int id, Integer meta, Selector parent) {
         this(id, meta, 1, parent);
     }
 
+    public EnchantmentItemSelector(int id, Integer meta, Selector parent, boolean randomDurability) {
+        this(id, meta, 1, parent, randomDurability);
+    }
+
     public EnchantmentItemSelector(int id, Integer meta, int count, Selector parent) {
         this(Item.get(id, meta, count), parent);
     }
 
+    public EnchantmentItemSelector(int id, Integer meta, int count, Selector parent, boolean randomDurability) {
+        this(Item.get(id, meta, count), parent, randomDurability);
+    }
+
     public EnchantmentItemSelector(Item item, Selector parent) {
-        super(item, parent);
+        super(item, parent, false);
+    }
+
+    public EnchantmentItemSelector(Item item, Selector parent, boolean randomDurability) {
+        super(item, parent, randomDurability);
     }
 
     @Override
     public Object select() {
-        Item newItem = item.clone();
+        Item result = item.clone();
         double random = ThreadLocalRandom.current().nextDouble();
         if (random < 0.1) {
             for (int i = 0; i < 3; i++) {
-                endowRandomEnchantmentByItem(newItem);
+                endowRandomEnchantmentByItem(result);
             }
         } else if (random < 0.2) {
             for (int i = 0; i < 2; i++) {
-                endowRandomEnchantmentByItem(newItem);
+                endowRandomEnchantmentByItem(result);
             }
         } else {
-            endowRandomEnchantmentByItem(newItem);
+            endowRandomEnchantmentByItem(result);
         }
-        return newItem;
+        if (randomDurability) {
+            result.setDamage(result.getMaxDurability() - Utils.rand(0, result.getMaxDurability() - 1));
+        }
+        return result;
     }
 
     public void endowRandomEnchantmentByItem(Item randomItem) {
