db22ef7f32f4ce8d578240cbceacf182600febc6
Glorydark <61799945+glorydark@users.noreply.github.com>
2024-06-23 22:40:41 +0800

    Fishing Can Harvest Tools With Random Damage (#283)

--- src/main/java/cn/nukkit/item/randomitem/ConstantItemSelector.java
+++ src/main/java/cn/nukkit/item/randomitem/ConstantItemSelector.java
@@ -1,6 +1,7 @@
 package cn.nukkit.item.randomitem;
 
 import cn.nukkit.item.Item;
+import cn.nukkit.utils.Utils;
 
 /**
  * Created by Snake1999 on 2016/1/15.
@@ -10,21 +11,36 @@ public class ConstantItemSelector extends Selector {
 
     protected final Item item;
 
+    protected final boolean randomDurability;
+
     public ConstantItemSelector(int id, Selector parent) {
-        this(id, 0, parent);
+        this(id, 0, parent, true);
+    }
+
+    public ConstantItemSelector(int id, Selector parent, boolean randomDurability) {
+        this(id, 0, parent, randomDurability);
     }
 
     public ConstantItemSelector(int id, Integer meta, Selector parent) {
-        this(id, meta, 1, parent);
+        this(id, meta, 1, parent, false);
+    }
+
+    public ConstantItemSelector(int id, Integer meta, Selector parent, boolean randomDurability) {
+        this(id, meta, 1, parent, randomDurability);
     }
 
     public ConstantItemSelector(int id, Integer meta, int count, Selector parent) {
-        this(Item.get(id, meta, count), parent);
+        this(Item.get(id, meta, count), parent, false);
     }
 
-    public ConstantItemSelector(Item item, Selector parent) {
+    public ConstantItemSelector(int id, Integer meta, int count, Selector parent, boolean randomDurability) {
+        this(Item.get(id, meta, count), parent, randomDurability);
+    }
+
+    public ConstantItemSelector(Item item, Selector parent, boolean randomDurability) {
         super(parent);
         this.item = item;
+        this.randomDurability = randomDurability;
     }
 
     public Item getItem() {
@@ -33,6 +49,10 @@ public class ConstantItemSelector extends Selector {
 
     @Override
     public Object select() {
-        return item;
+        Item result = item.clone();
+        if (this.randomDurability) {
+            result.setDamage(result.getMaxDurability() - Utils.rand(0, result.getMaxDurability() - 1));
+        }
+        return result;
     }
 }
