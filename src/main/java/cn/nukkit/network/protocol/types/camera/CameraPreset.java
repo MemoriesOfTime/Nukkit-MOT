5262ec66c1633c9aa0f780449e5860e22044d745
LT_Name <572413378@qq.com>
2024-08-15 21:54:49 +0800

    Stuff

--- src/main/java/cn/nukkit/network/protocol/types/camera/CameraPreset.java
+++ src/main/java/cn/nukkit/network/protocol/types/camera/CameraPreset.java
@@ -1,15 +1,14 @@
 package cn.nukkit.network.protocol.types.camera;
 
+import cn.nukkit.math.Vector2f;
 import cn.nukkit.math.Vector3f;
-import cn.nukkit.utils.CameraPresetManager;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.cloudburstmc.protocol.common.NamedDefinition;
 import org.cloudburstmc.protocol.common.util.OptionalBoolean;
-
-import javax.annotation.Nullable;
+import org.jetbrains.annotations.Nullable;
 
 /**
  * @author daoge_cmd
@@ -30,6 +29,14 @@ public final class CameraPreset implements NamedDefinition {
     private Float yaw;
     @Nullable
     private Float pitch;
+    /**
+     * @since v712
+     */
+    private Vector2f viewOffset;
+    /**
+     * @since v712
+     */
+    private Float radius;
     @Nullable
     private CameraAudioListener listener;
     private OptionalBoolean playEffect;
@@ -42,6 +49,18 @@ public final class CameraPreset implements NamedDefinition {
         this.pos = pos;
         this.yaw = yaw;
         this.pitch = pitch;
+        this.listener = listener;
+        this.playEffect = playEffect;
+    }
+
+    public CameraPreset(String identifier, String parentPreset, @Nullable Vector3f pos, @Nullable Float yaw, @Nullable Float pitch, @Nullable Vector2f viewOffset, @Nullable Float radius, @Nullable CameraAudioListener listener, OptionalBoolean playEffect) {
+        this.identifier = identifier;
+        this.parentPreset = parentPreset;
+        this.pos = pos;
+        this.yaw = yaw;
+        this.pitch = pitch;
+        this.viewOffset = viewOffset;
+        this.radius = radius;
         this.listener = listener;
         this.playEffect = playEffect;
     }
