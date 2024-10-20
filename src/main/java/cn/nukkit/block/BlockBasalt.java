ac2a62e645ca2b26de13ef176e595c77464519ce
Koshak_Mine <52908036+KoshakMineDEV@users.noreply.github.com>
2024-10-20 20:45:58 +0300

    fix: basalt rotation

--- src/main/java/cn/nukkit/block/BlockBasalt.java
+++ src/main/java/cn/nukkit/block/BlockBasalt.java
@@ -1,6 +1,9 @@
 package cn.nukkit.block;
 
+import cn.nukkit.Player;
+import cn.nukkit.item.Item;
 import cn.nukkit.item.ItemTool;
+import cn.nukkit.math.BlockFace;
 
 public class BlockBasalt extends BlockSolidMeta {
     public BlockBasalt() {
@@ -19,6 +22,38 @@ public class BlockBasalt extends BlockSolidMeta {
     @Override
     public double getResistance() {
         return 4.2;
+    }
+
+    @Override
+    public boolean place(Item item, Block block, Block target, BlockFace face, double fx, double fy, double fz, Player player) {
+        this.setPillarAxis(face.getAxis());
+        this.getLevel().setBlock(block, this, true, true);
+        return true;
+    }
+    
+    public void setPillarAxis(BlockFace.Axis axis) {
+        switch (axis) {
+            case Y:
+                this.setDamage(0);
+                break;
+            case X:
+                this.setDamage(1);
+                break;
+            case Z:
+                this.setDamage(2);
+                break;
+        }
+    }
+    public BlockFace.Axis getPillarAxis() {
+        switch (this.getDamage() % 3) {
+            case 2:
+                return BlockFace.Axis.Z;
+            case 1:
+                return BlockFace.Axis.X;
+            case 0:
+            default:
+                return BlockFace.Axis.Y;
+        }
     }
 
     @Override
