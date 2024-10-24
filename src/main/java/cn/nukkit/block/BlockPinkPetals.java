cd119a19d538b7e6f745ff108e0e7836a098a352
LT_Name <572413378@qq.com>
2024-10-24 11:56:47 +0800

    fix

--- src/main/java/cn/nukkit/block/BlockPinkPetals.java
+++ src/main/java/cn/nukkit/block/BlockPinkPetals.java
@@ -1,44 +1,30 @@
 package cn.nukkit.block;
 
 import cn.nukkit.Player;
-import cn.nukkit.block.properties.BlockProperties;
+import cn.nukkit.block.custom.properties.BlockProperties;
+import cn.nukkit.block.custom.properties.IntBlockProperty;
 import cn.nukkit.block.properties.BlockPropertiesHelper;
-import cn.nukkit.block.properties.EnumBlockProperty;
-import cn.nukkit.block.properties.IntBlockProperty;
+import cn.nukkit.block.properties.VanillaProperties;
 import cn.nukkit.item.Item;
+import cn.nukkit.level.particle.BoneMealParticle;
 import cn.nukkit.math.BlockFace;
+import cn.nukkit.utils.Faceable;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-
-public class BlockPinkPetals extends BlockFlowable implements BlockPropertiesHelper {
-
-    public enum MINECRAFT_CARDINAL_DIRECTION {
-        SOUTH,
-        WEST,
-        NORTH,
-        EAST
-    }
-
-    @Deprecated
-    private static final IntBlockProperty DIRECTION = new IntBlockProperty("direction", false, 3, 0);
+public class BlockPinkPetals extends BlockFlowable implements Faceable, BlockPropertiesHelper {
 
     private static final IntBlockProperty GROWTH = new IntBlockProperty("growth", false, 7, 0);
 
-    private static final EnumBlockProperty<MINECRAFT_CARDINAL_DIRECTION> CARDINAL_DIRECTION = new EnumBlockProperty<>("minecraft:cardinal_direction", false, MINECRAFT_CARDINAL_DIRECTION.values());
-
-    private static final BlockProperties PROPERTIES = new BlockProperties(GROWTH, CARDINAL_DIRECTION);
-
-
-    public MINECRAFT_CARDINAL_DIRECTION cardinalDirection = MINECRAFT_CARDINAL_DIRECTION.values()[0];
+    private static final BlockProperties PROPERTIES = new BlockProperties(GROWTH, VanillaProperties.CARDINAL_DIRECTION);
 
     public BlockPinkPetals() {
-            this(0);
-        }
+        this(0);
+    }
 
     public BlockPinkPetals(int meta) {
-            super(meta);
-        }
+        super(meta);
+    }
 
     @Override
     public String getName() {
@@ -50,21 +36,34 @@ public class BlockPinkPetals extends BlockFlowable implements BlockPropertiesHel
         return BlockID.PINK_PETALS;
     }
 
-    public void setCardinalDirection(int cardinalDirectionIndex) {
-        // 检查索引是否在枚举的范围内
-        if (cardinalDirectionIndex >= 0 && cardinalDirectionIndex < MINECRAFT_CARDINAL_DIRECTION.values().length) {
-            this.cardinalDirection = MINECRAFT_CARDINAL_DIRECTION.values()[cardinalDirectionIndex];
-        } else {
-            throw new IllegalArgumentException("Invalid cardinal direction index: " + cardinalDirectionIndex);
-        }
+    @Override
+    public String getIdentifier() {
+        return "minecraft:pink_petals";
+    }
+
+    @Override
+    public BlockProperties getBlockProperties() {
+        return PROPERTIES;
+    }
+
+    @Override
+    public BlockFace getBlockFace() {
+        return this.getPropertyValue(VanillaProperties.CARDINAL_DIRECTION);
+    }
+
+    @Override
+    public void setBlockFace(BlockFace face) {
+        this.setPropertyValue(VanillaProperties.CARDINAL_DIRECTION, face);
     }
 
     @Override
     public boolean place(@NotNull Item item, @NotNull Block block, @NotNull Block target, @NotNull BlockFace face, double fx, double fy, double fz, @Nullable Player player) {
-        if (!isSupportValid(block.down()))
+        if (!isSupportValid(block.down())) {
             return false;
-        if (player != null)
-            setCardinalDirection(player.getHorizontalFacing().getOpposite().getIndex());
+        }
+        if (player != null) {
+            setBlockFace(player.getHorizontalFacing().getOpposite());
+        }
         return this.getLevel().setBlock(this, this);
     }
 
@@ -88,29 +87,26 @@ public class BlockPinkPetals extends BlockFlowable implements BlockPropertiesHel
     }
 
     @Override
-    public BlockProperties getBlockProperties() {
-        return PROPERTIES;
-    }
-
-    @Override
     public boolean onActivate(@NotNull Item item, @Nullable Player player) {
-//        if (item.isFertilizer()) {
-//            if (getPropertyValue(GROWTH) < 3) {
-//                setPropertyValue(GROWTH, getPropertyValue(GROWTH) + 1);
-//                getLevel().setBlock(this, this);
-//            } else {
-//                getLevel().dropItem(this, Block.get(Item.PINK_PETALS).toItem());
-//            }
-//            this.level.addParticle(new BoneMealParticle(this));
-//            item.count--;
-//            return true;
-//        }
-        if (item.getId() == Item.fromString("minecraft:pink_petals").getId() && getPropertyValue(GROWTH) < 3) {
-            setPropertyValue(GROWTH, getPropertyValue(GROWTH) + 1);
+        if (item.getId() == Item.DYE && item.getDamage() == 0x0f) {
+            if (this.getPropertyValue(GROWTH) < 3) {
+                setPropertyValue(GROWTH, this.getPropertyValue(GROWTH) + 1);
+                getLevel().setBlock(this, this);
+            } else {
+                getLevel().dropItem(this, this.toItem());
+            }
+            this.level.addParticle(new BoneMealParticle(this));
+            item.count--;
+            return true;
+        }
+
+        if (item.getId() == this.getItemId() && this.getPropertyValue(GROWTH) < 3) {
+            setPropertyValue(GROWTH, this.getPropertyValue(GROWTH) + 1);
             getLevel().setBlock(this, this);
             item.count--;
             return true;
         }
+
         return false;
     }
 }
