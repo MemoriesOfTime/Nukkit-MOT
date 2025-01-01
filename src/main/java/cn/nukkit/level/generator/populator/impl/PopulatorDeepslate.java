612ed3ac2072bee761182f55ea33f34273ea851b
KoshakMineDEV <52908036+koshakminedev@users.noreply.github.com>
2025-01-01 18:34:34 +0300

    feat: NewNormal generator

--- src/main/java/cn/nukkit/level/generator/populator/impl/PopulatorDeepslate.java
+++ src/main/java/cn/nukkit/level/generator/populator/impl/PopulatorDeepslate.java
@@ -7,15 +7,18 @@ import cn.nukkit.math.NukkitRandom;
 
 
 public class PopulatorDeepslate extends Populator {
+    private final int bedrockLayer;
+
+    public PopulatorDeepslate(int bedrockLayer) {
+        this.bedrockLayer = bedrockLayer;
+    }
 
     @Override
     public void populate(ChunkManager level, int chunkX, int chunkZ, NukkitRandom random, FullChunk chunk) {
         for (int x = 0; x < 16; x++) {
             for (int z = 0; z < 16; z++) {
-                for (int y = 1; y < 8; y++) {
-                    if (random.nextBoundedInt(y + 1) == 0) {
-                        chunk.setBlockId(x, y, z, DEEPSLATE);
-                    }
+                for(int y = bedrockLayer; y < 0; y++) {
+                    chunk.setBlockId(x, y, z, DEEPSLATE);
                 }
             }
         }
