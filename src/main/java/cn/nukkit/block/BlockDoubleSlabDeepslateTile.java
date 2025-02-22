4efc1ef8dca26e09c19e0d89dec5904f00e088be
Koshak_Mine <52908036+KoshakMineDEV@users.noreply.github.com>
2025-02-22 07:52:54 +0300

    feat: more blocks (#418)

---
+++ src/main/java/cn/nukkit/block/BlockDoubleSlabDeepslateTile.java
@@ -0,0 +1,58 @@
+package cn.nukkit.block;
+
+import cn.nukkit.item.Item;
+import cn.nukkit.item.ItemBlock;
+import cn.nukkit.item.ItemTool;
+
+public class BlockDoubleSlabDeepslateTile extends BlockSolidMeta {
+
+    public BlockDoubleSlabDeepslateTile() {
+        this(0);
+    }
+
+    public BlockDoubleSlabDeepslateTile(int meta) {
+        super(meta);
+    }
+
+    @Override
+    public int getId() {
+        return DEEPSLATE_TILE_DOUBLE_SLAB;
+    }
+
+    @Override
+    public double getHardness() {
+        return 3.5;
+    }
+
+    @Override
+    public double getResistance() {
+        return 15;
+    }
+
+    @Override
+    public int getToolType() {
+        return ItemTool.TYPE_PICKAXE;
+    }
+
+    @Override
+    public int getToolTier() {
+        return ItemTool.TIER_WOODEN;
+    }
+
+    @Override
+    public String getName() {
+        return "Cobbled Deepslate Slab";
+    }
+
+    @Override
+    public Item toItem() {
+        return new ItemBlock(Block.get(DEEPSLATE_TILE_SLAB));
+    }
+
+    @Override
+    public Item[] getDrops(Item item) {
+        return new Item[]{
+                Item.get(Item.DEEPSLATE_TILE_SLAB, 0, 2)
+        };
+    }
+}
