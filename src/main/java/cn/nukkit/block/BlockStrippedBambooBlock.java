cb4411c9d55fcbae5208c1404c05147c07c67eff
Koshak_Mine <52908036+KoshakMineDEV@users.noreply.github.com>
2025-02-23 15:28:42 +0300

    feat: more blocks (#420)

---
+++ src/main/java/cn/nukkit/block/BlockStrippedBambooBlock.java
@@ -0,0 +1,47 @@
+package cn.nukkit.block;
+
+import cn.nukkit.item.Item;
+
+public class BlockStrippedBambooBlock extends BlockWoodStripped {
+    public BlockStrippedBambooBlock() {
+        super(0);
+    }
+
+    public BlockStrippedBambooBlock(int meta) {
+        super(meta);
+    }
+
+
+    public int getId() {
+        return STRIPPED_BAMBOO_BLOCK;
+    }
+
+    public String getName() {
+        return "Stripped Bamboo Block";
+    }
+
+    @Override
+    public double getHardness() {
+        return 2;
+    }
+
+    @Override
+    public double getResistance() {
+        return 15;
+    }
+
+    @Override
+    public int getBurnChance() {
+        return 5;
+    }
+
+    @Override
+    public int getBurnAbility() {
+        return 20;
+    }
+
+    @Override
+    protected int getStrippedId() {
+        return STRIPPED_BAMBOO_BLOCK;
+    }
+}
