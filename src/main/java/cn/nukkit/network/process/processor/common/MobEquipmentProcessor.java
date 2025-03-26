eccfa3b6066c40e5aaf29adb4eab183b678880b9
Alexander Tyrin <labarjni@gmail.com>
2025-03-27 02:21:22 +0300

    rollback, not related

--- src/main/java/cn/nukkit/network/process/processor/common/MobEquipmentProcessor.java
+++ src/main/java/cn/nukkit/network/process/processor/common/MobEquipmentProcessor.java
@@ -44,14 +44,14 @@ public class MobEquipmentProcessor extends DataPacketProcessor<MobEquipmentPacke
 
         Item item = inv.getItem(pk.hotbarSlot);
 
-        if (!item.equals(pk.item, false, true)) {
-            Item fixItem = Item.get(item.getId(), item.getDamage(), item.getCount(), item.getCompoundTag());
-            if (fixItem.equals(pk.item, false, true)) {
-                inv.setItem(pk.hotbarSlot, fixItem);
-            } else {
-                player.getServer().getLogger().debug("Tried to equip "+pk.item+" but have {} in target slot "+fixItem);
-                inv.sendContents(player);
+        if (!item.equals(pk.item)) {
+            player.getServer().getLogger().debug(player.getName() + " tried to equip " + pk.item + " but have " + item + " in target slot");
+            playerHandle.setFailedMobEquipmentPacket(playerHandle.getFailedMobEquipmentPacket() + 1);
+            if (playerHandle.getFailedMobEquipmentPacket() > MAX_FAILED) {
+                player.close("", "Too many failed packets");
             }
+            inv.sendContents(player);
+            return;
         }
 
         if (inv instanceof PlayerInventory) {
