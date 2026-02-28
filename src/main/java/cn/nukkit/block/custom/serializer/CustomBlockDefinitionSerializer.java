1ce4364b66e3013fb81811fa4ebcd7df52e1b72c
LT_Name <572413378@qq.com>
2026-02-28 22:36:53 +0800

    refactor: optimize custom block definition serializer chain with v843 material conversion and permutation support

---
+++ src/main/java/cn/nukkit/block/custom/serializer/CustomBlockDefinitionSerializer.java
@@ -0,0 +1,31 @@
+package cn.nukkit.block.custom.serializer;
+
+import cn.nukkit.block.custom.serializer.impl.CustomBlockDefinitionSerializer843;
+import cn.nukkit.block.custom.serializer.impl.CustomBlockDefinitionSerializer898;
+import cn.nukkit.nbt.tag.CompoundTag;
+import cn.nukkit.network.protocol.ProtocolInfo;
+
+public class CustomBlockDefinitionSerializer {
+
+    public CompoundTag serialize(CompoundTag nbt) {
+        reSerializeMaterials(nbt);
+        reSerializeCollisionBox(nbt);
+        return nbt;
+    }
+
+    protected void reSerializeMaterials(CompoundTag nbt) {
+    }
+
+    protected void reSerializeCollisionBox(CompoundTag nbt) {
+    }
+
+    public static CompoundTag serialize(CompoundTag nbt, int protocol) {
+        if (protocol >= ProtocolInfo.v1_21_130) {
+            return CustomBlockDefinitionSerializer898.INSTANCE.serialize(nbt.clone());
+        }
+        if (protocol >= ProtocolInfo.v1_21_110_26) {
+            return CustomBlockDefinitionSerializer843.INSTANCE.serialize(nbt.clone());
+        }
+        return nbt;
+    }
+}
---
+++ src/main/java/cn/nukkit/block/custom/serializer/impl/CustomBlockDefinitionSerializer898.java
@@ -0,0 +1,73 @@
+package cn.nukkit.block.custom.serializer.impl;
+
+import cn.nukkit.nbt.tag.CompoundTag;
+import cn.nukkit.nbt.tag.FloatTag;
+import cn.nukkit.nbt.tag.ListTag;
+import cn.nukkit.nbt.tag.Tag;
+
+/**
+ * v898+ (1.21.130): collision_box origin/size -> boxes(min/max)
+ */
+public class CustomBlockDefinitionSerializer898 extends CustomBlockDefinitionSerializer843 {
+
+    public static final CustomBlockDefinitionSerializer898 INSTANCE = new CustomBlockDefinitionSerializer898();
+
+    @Override
+    protected void reSerializeCollisionBox(CompoundTag nbt) {
+        CompoundTag components = nbt.getCompound("components");
+        convertCollisionBox(components);
+
+        if (nbt.contains("permutations")) {
+            for (Tag tag : nbt.getList("permutations", CompoundTag.class).getAll()) {
+                CompoundTag permutation = (CompoundTag) tag;
+                if (permutation.contains("components")) {
+                    convertCollisionBox(permutation.getCompound("components"));
+                }
+            }
+        }
+    }
+
+    private static void convertCollisionBox(CompoundTag components) {
+        if (!components.contains("minecraft:collision_box")) {
+            return;
+        }
+
+        CompoundTag collisionBox = components.getCompound("minecraft:collision_box");
+        if (!collisionBox.contains("origin") || !collisionBox.contains("size")) {
+            return;
+        }
+
+        ListTag<FloatTag> origin = collisionBox.getList("origin", FloatTag.class);
+        ListTag<FloatTag> size = collisionBox.getList("size", FloatTag.class);
+
+        if (origin.isEmpty() || size.isEmpty()) {
+            return;
+        }
+
+        float originX = origin.get(0).parseValue();
+        float originY = origin.get(1).parseValue();
+        float originZ = origin.get(2).parseValue();
+        float sizeX = size.get(0).parseValue();
+        float sizeY = size.get(1).parseValue();
+        float sizeZ = size.get(2).parseValue();
+
+        float minX = originX + 8f;
+        float minY = originY;
+        float minZ = originZ + 8f;
+        float maxX = minX + sizeX;
+        float maxY = minY + sizeY;
+        float maxZ = minZ + sizeZ;
+
+        CompoundTag box = new CompoundTag()
+                .putFloat("minX", minX)
+                .putFloat("minY", minY)
+                .putFloat("minZ", minZ)
+                .putFloat("maxX", maxX)
+                .putFloat("maxY", maxY)
+                .putFloat("maxZ", maxZ);
+
+        collisionBox.remove("origin");
+        collisionBox.remove("size");
+        collisionBox.putList("boxes", new ListTag<CompoundTag>().add(box));
+    }
+}
