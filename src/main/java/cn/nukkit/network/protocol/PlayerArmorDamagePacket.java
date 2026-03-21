7408ae7ff7fd698183deeceb62191ff005087271
LT_Name <572413378@qq.com>
2026-03-21 09:41:44 +0800

    fix

--- src/main/java/cn/nukkit/network/protocol/PlayerArmorDamagePacket.java
+++ src/main/java/cn/nukkit/network/protocol/PlayerArmorDamagePacket.java
@@ -20,14 +20,21 @@ public class PlayerArmorDamagePacket extends DataPacket {
 
     @Override
     public void decode() {
-        int flagsval = this.getByte();
-        for (int i = 0; i < 5; i++) {
-            if (i == 4 && this.protocol < ProtocolInfo.v1_21_20) {
-                continue;
-            }
-            if ((flagsval & (1 << i)) != 0) {
-                this.flags.add(PlayerArmorDamageFlag.values()[i]);
-                this.damage[i] = this.getVarInt();
+        if (this.protocol >= ProtocolInfo.v1_21_110) {
+            this.getArray(this.flags, stream -> {
+                int ordinal = stream.getVarInt();
+                PlayerArmorDamageFlag flag = PlayerArmorDamageFlag.values()[ordinal];
+                this.damage[ordinal] = stream.getLShort();
+                return flag;
+            });
+        } else {
+            int flagsval = this.getByte();
+            int maxIndex = this.protocol >= ProtocolInfo.v1_21_20 ? 4 : 3;
+            for (int i = 0; i <= maxIndex; i++) {
+                if ((flagsval & (1 << i)) != 0) {
+                    this.flags.add(PlayerArmorDamageFlag.values()[i]);
+                    this.damage[i] = this.getVarInt();
+                }
             }
         }
     }
@@ -35,20 +42,27 @@ public class PlayerArmorDamagePacket extends DataPacket {
     @Override
     public void encode() {
         this.reset();
-        int outflags = 0;
-        for (PlayerArmorDamageFlag flag : this.flags) {
-            if (flag == PlayerArmorDamageFlag.BODY && this.protocol < ProtocolInfo.v1_21_20) {
-                continue;
+        if (this.protocol >= ProtocolInfo.v1_21_110) {
+            this.putArray(this.flags, flag -> {
+                this.putVarInt(flag.ordinal());
+                this.putLShort(this.damage[flag.ordinal()]);
+            });
+        } else {
+            int outflags = 0;
+            for (PlayerArmorDamageFlag flag : this.flags) {
+                if (flag == PlayerArmorDamageFlag.BODY && this.protocol < ProtocolInfo.v1_21_20) {
+                    continue;
+                }
+                outflags |= 1 << flag.ordinal();
             }
-            outflags |= 1 << flag.ordinal();
-        }
-        this.putByte((byte) outflags);
+            this.putByte((byte) outflags);
 
-        for (PlayerArmorDamageFlag flag : this.flags) {
-            if (flag == PlayerArmorDamageFlag.BODY && this.protocol < ProtocolInfo.v1_21_20) {
-                continue;
+            for (PlayerArmorDamageFlag flag : this.flags) {
+                if (flag == PlayerArmorDamageFlag.BODY && this.protocol < ProtocolInfo.v1_21_20) {
+                    continue;
+                }
+                this.putVarInt(this.damage[flag.ordinal()]);
             }
-            this.putVarInt(this.damage[flag.ordinal()]);
         }
     }
 
