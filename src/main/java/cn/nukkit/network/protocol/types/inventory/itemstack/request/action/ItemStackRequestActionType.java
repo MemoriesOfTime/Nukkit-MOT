ae314c2ae83fe6aa95081d6e143a2cc2a9b12374
LT_Name <572413378@qq.com>
2026-04-11 13:50:49 +0800

    fix

--- src/main/java/cn/nukkit/network/protocol/types/inventory/itemstack/request/action/ItemStackRequestActionType.java
+++ src/main/java/cn/nukkit/network/protocol/types/inventory/itemstack/request/action/ItemStackRequestActionType.java
@@ -5,6 +5,7 @@ import cn.nukkit.network.protocol.ProtocolInfo;
 import it.unimi.dsi.fastutil.ints.Int2ObjectArrayMap;
 
 public enum ItemStackRequestActionType {
+
     TAKE(0),
     PLACE(1),
     SWAP(2),
@@ -25,6 +26,7 @@ public enum ItemStackRequestActionType {
     CRAFT_LOOM(17),
     CRAFT_NON_IMPLEMENTED_DEPRECATED(18),
     CRAFT_RESULTS_DEPRECATED(19);
+
     private final int id;
 
     private static final Int2ObjectArrayMap<ItemStackRequestActionType> VALUES = new Int2ObjectArrayMap<>();
@@ -48,97 +50,10 @@ public enum ItemStackRequestActionType {
     }
 
     public static ItemStackRequestActionType fromId(int id, GameVersion gameVersion) {
-        int protocol = gameVersion.getProtocol();
-        if (protocol >= ProtocolInfo.v1_21_20) {
-            return switch (id) {
-                case 0 -> TAKE;
-                case 1 -> PLACE;
-                case 2 -> SWAP;
-                case 3 -> DROP;
-                case 4 -> DESTROY;
-                case 5 -> CONSUME;
-                case 6 -> CREATE;
-                case 9 -> LAB_TABLE_COMBINE;
-                case 10 -> BEACON_PAYMENT;
-                case 11 -> MINE_BLOCK;
-                case 12 -> CRAFT_RECIPE;
-                case 13 -> CRAFT_RECIPE_AUTO;
-                case 14 -> CRAFT_CREATIVE;
-                case 15 -> CRAFT_RECIPE_OPTIONAL;
-                case 16 -> CRAFT_REPAIR_AND_DISENCHANT;
-                case 17 -> CRAFT_LOOM;
-                case 18 -> CRAFT_NON_IMPLEMENTED_DEPRECATED;
-                case 19 -> CRAFT_RESULTS_DEPRECATED;
-                default -> null;
-            };
-        }
-        if (protocol >= ProtocolInfo.v1_18_10_26) {
+        if (gameVersion.getProtocol() >= ProtocolInfo.v1_18_10_26) {
             return fromId(id);
         }
-        if (protocol >= ProtocolInfo.v1_17_40) {
-            return switch (id) {
-                case 0 -> TAKE;
-                case 1 -> PLACE;
-                case 2 -> SWAP;
-                case 3 -> DROP;
-                case 4 -> DESTROY;
-                case 5 -> CONSUME;
-                case 6 -> CREATE;
-                case 7 -> LAB_TABLE_COMBINE;
-                case 8 -> BEACON_PAYMENT;
-                case 9 -> MINE_BLOCK;
-                case 10 -> CRAFT_RECIPE;
-                case 11 -> CRAFT_RECIPE_AUTO;
-                case 12 -> CRAFT_CREATIVE;
-                case 13 -> CRAFT_RECIPE_OPTIONAL;
-                case 14 -> CRAFT_REPAIR_AND_DISENCHANT;
-                case 15 -> CRAFT_LOOM;
-                case 16 -> CRAFT_NON_IMPLEMENTED_DEPRECATED;
-                case 17 -> CRAFT_RESULTS_DEPRECATED;
-                default -> null;
-            };
-        }
-        if (protocol >= ProtocolInfo.v1_16_210) {
-            return switch (id) {
-                case 0 -> TAKE;
-                case 1 -> PLACE;
-                case 2 -> SWAP;
-                case 3 -> DROP;
-                case 4 -> DESTROY;
-                case 5 -> CONSUME;
-                case 6 -> CREATE;
-                case 7 -> LAB_TABLE_COMBINE;
-                case 8 -> BEACON_PAYMENT;
-                case 9 -> MINE_BLOCK;
-                case 10 -> CRAFT_RECIPE;
-                case 11 -> CRAFT_RECIPE_AUTO;
-                case 12 -> CRAFT_CREATIVE;
-                case 13 -> CRAFT_RECIPE_OPTIONAL;
-                case 14 -> CRAFT_NON_IMPLEMENTED_DEPRECATED;
-                case 15 -> CRAFT_RESULTS_DEPRECATED;
-                default -> null;
-            };
-        }
-        if (protocol >= ProtocolInfo.v1_16_200) {
-            return switch (id) {
-                case 0 -> TAKE;
-                case 1 -> PLACE;
-                case 2 -> SWAP;
-                case 3 -> DROP;
-                case 4 -> DESTROY;
-                case 5 -> CONSUME;
-                case 6 -> CREATE;
-                case 7 -> LAB_TABLE_COMBINE;
-                case 8 -> BEACON_PAYMENT;
-                case 9 -> CRAFT_RECIPE;
-                case 10 -> CRAFT_RECIPE_AUTO;
-                case 11 -> CRAFT_CREATIVE;
-                case 12 -> CRAFT_RECIPE_OPTIONAL;
-                case 13 -> CRAFT_NON_IMPLEMENTED_DEPRECATED;
-                case 14 -> CRAFT_RESULTS_DEPRECATED;
-                default -> null;
-            };
-        }
+        int protocol = gameVersion.getProtocol();
         return switch (id) {
             case 0 -> TAKE;
             case 1 -> PLACE;
@@ -149,11 +64,20 @@ public enum ItemStackRequestActionType {
             case 6 -> CREATE;
             case 7 -> LAB_TABLE_COMBINE;
             case 8 -> BEACON_PAYMENT;
-            case 9 -> CRAFT_RECIPE;
-            case 10 -> CRAFT_RECIPE_AUTO;
-            case 11 -> CRAFT_CREATIVE;
-            case 12 -> CRAFT_NON_IMPLEMENTED_DEPRECATED;
-            case 13 -> CRAFT_RESULTS_DEPRECATED;
+            case 9 -> protocol >= ProtocolInfo.v1_16_210 ? MINE_BLOCK : CRAFT_RECIPE;
+            case 10 -> protocol >= ProtocolInfo.v1_16_210 ? CRAFT_RECIPE : CRAFT_RECIPE_AUTO;
+            case 11 -> protocol >= ProtocolInfo.v1_16_210 ? CRAFT_RECIPE_AUTO : CRAFT_CREATIVE;
+            case 12 -> protocol >= ProtocolInfo.v1_16_210 ? CRAFT_CREATIVE
+                    : protocol >= ProtocolInfo.v1_16_200 ? CRAFT_RECIPE_OPTIONAL : CRAFT_NON_IMPLEMENTED_DEPRECATED;
+            case 13 -> protocol >= ProtocolInfo.v1_16_210 ? CRAFT_RECIPE_OPTIONAL
+                    : protocol >= ProtocolInfo.v1_16_200 ? CRAFT_NON_IMPLEMENTED_DEPRECATED : CRAFT_RESULTS_DEPRECATED;
+            case 14 -> protocol >= ProtocolInfo.v1_17_40 ? CRAFT_REPAIR_AND_DISENCHANT
+                    : protocol >= ProtocolInfo.v1_16_210 ? CRAFT_NON_IMPLEMENTED_DEPRECATED
+                    : protocol >= ProtocolInfo.v1_16_200 ? CRAFT_RESULTS_DEPRECATED : null;
+            case 15 -> protocol >= ProtocolInfo.v1_17_40 ? CRAFT_LOOM
+                    : protocol >= ProtocolInfo.v1_16_210 ? CRAFT_RESULTS_DEPRECATED : null;
+            case 16 -> protocol >= ProtocolInfo.v1_17_40 ? CRAFT_NON_IMPLEMENTED_DEPRECATED : null;
+            case 17 -> protocol >= ProtocolInfo.v1_17_40 ? CRAFT_RESULTS_DEPRECATED : null;
             default -> null;
         };
     }
