MemoriesOfTime/Nukkit-MOT

commit 8470a571b08b9412e07a264ccbc96c8b20321ee5

fix: 384 height (#344)

src/main/java/cn/nukkit/level/Level.java
@@ -231,11 +231,11 @@ public class Level implements ChunkManager, Metadatable {
     private final String folderName;
 
     // Avoid OOM, gc'd references result in whole chunk being sent (possibly higher cpu)
-    private final Long2ObjectOpenHashMap<SoftReference<Map<Character, Object>>> changedBlocks = new Long2ObjectOpenHashMap<>();
+    private final Long2ObjectOpenHashMap<SoftReference<Map<Integer, Object>>> changedBlocks = new Long2ObjectOpenHashMap<>();
     // Storing the vector is redundant
     private final Object changeBlocksPresent = new Object();
     // Storing extra blocks past 512 is redundant
-    private final Map<Character, Object> changeBlocksFullMap = new CharacterHashMap();
+    private final Int2ObjectOpenHashMap<Object> changeBlocksFullMap = new Int2ObjectOpenHashMap<>();
 
     private final BlockUpdateScheduler updateQueue;
     private final Queue<QueuedUpdate> normalUpdateQueue = new ConcurrentLinkedDeque<>();
@@ -406,23 +406,45 @@ public class Level implements ChunkManager, Metadatable {
         return (((long) x) << 32) | (z & 0xffffffffL);
     }
 
+    @Deprecated
     public static long blockHash(int x, int y, int z) {
-        if (y < 0 || y >= 256) {
+        if (y < -64 || y >= 384) {
             throw new IllegalArgumentException("Y coordinate " + y + " is out of range!");
         }
-        return (((long) x & (long) 0xFFFFFFF) << 36) | (((long) y & (long) 0xFF) << 28) | ((long) z & (long) 0xFFFFFFF);
+        return blockHash(x, y, z, DimensionEnum.OVERWORLD.getDimensionData());
     }
 
+    public static long blockHash(int x, int y, int z, DimensionData dimensionData) {
+        return (((long) x & (long) 0xFFFFFFF) << 36) | ((long) (capWorldY(y, dimensionData) - dimensionData.getMinHeight()) << 28) | ((long) z & (long) 0xFFFFFFF);
+    }
+
+    @Deprecated
     public static char localBlockHash(double x, double y, double z) {
         byte hi = (byte) (((int) x & 15) + (((int) z & 15) << 4));
         byte lo = (byte) y;
         return (char) (((hi & 0xFF) << 8) | (lo & 0xFF));
     }
 
+    public static int localBlockHash(double x, double y, double z, DimensionData dimensionData) {
+        byte hi = (byte) (((int) x & 15) + (((int) z & 15) << 4));
+        short lo = (short) (capWorldY((int) y, dimensionData) - dimensionData.getMinHeight());
+        return (hi & 0xFF) << 16 | lo;
+    }
+
+    @Deprecated
     public static Vector3 getBlockXYZ(long chunkHash, char blockHash) {
         int hi = (byte) (blockHash >>> 8);
         int lo = (byte) blockHash;
         int y = lo & 0xFF;
+        int x = (hi & 0xF) + (getHashX(chunkHash) << 4);
+        int z = ((hi >> 4) & 0xF) + (getHashZ(chunkHash) << 4);
+        return new Vector3(x, y, z);
+    }
+
+    public static Vector3 getBlockXYZ(long chunkHash, int blockHash, DimensionData dimensionData) {
+        int hi = (byte) (blockHash >>> 16);
+        int lo = (short) blockHash;
+        int y = capWorldY(lo + dimensionData.getMinHeight(), dimensionData);
         int x = (hi & 0xF) + (getHashX(chunkHash) << 4);
         int z = ((hi >> 4) & 0xF) + (getHashZ(chunkHash) << 4);
         return new Vector3(x, y, z);
@@ -450,6 +472,10 @@ public class Level implements ChunkManager, Metadatable {
 
     public static Chunk.Entry getChunkXZ(long hash) {
         return new Chunk.Entry(getHashX(hash), getHashZ(hash));
+    }
+
+    private static int capWorldY(int y, DimensionData dimensionData) {
+        return Math.max(Math.min(y, dimensionData.getMaxHeight()), dimensionData.getMinHeight());
     }
 
     public static int generateChunkLoaderId(ChunkLoader loader) {
@@ -1062,11 +1088,11 @@ public class Level implements ChunkManager, Metadatable {
         synchronized (changedBlocks) {
             if (!this.changedBlocks.isEmpty()) {
                 if (!this.players.isEmpty()) {
-                    ObjectIterator<Long2ObjectMap.Entry<SoftReference<Map<Character, Object>>>> iter = changedBlocks.long2ObjectEntrySet().fastIterator();
+                    ObjectIterator<Long2ObjectMap.Entry<SoftReference<Map<Integer, Object>>>> iter = changedBlocks.long2ObjectEntrySet().fastIterator();
                     while (iter.hasNext()) {
-                        Long2ObjectMap.Entry<SoftReference<Map<Character, Object>>> entry = iter.next();
+                        Long2ObjectMap.Entry<SoftReference<Map<Integer, Object>>> entry = iter.next();
                         long index = entry.getLongKey();
-                        Map<Character, Object> blocks = entry.getValue().get();
+                        Map<Integer, Object> blocks = entry.getValue().get();
                         int chunkX = Level.getHashX(index);
                         int chunkZ = Level.getHashZ(index);
                         if (blocks == null || blocks.size() > MAX_BLOCK_CACHE) {
@@ -1078,8 +1104,8 @@ public class Level implements ChunkManager, Metadatable {
                             Player[] playerArray = this.getChunkPlayers(chunkX, chunkZ).values().toArray(Player.EMPTY_ARRAY);
                             Vector3[] blocksArray = new Vector3[blocks.size()];
                             int i = 0;
-                            for (char blockHash : blocks.keySet()) {
-                                Vector3 hash = getBlockXYZ(index, blockHash);
+                            for (int blockHash : blocks.keySet()) {
+                                Vector3 hash = getBlockXYZ(index, blockHash, this.getDimensionData());
                                 blocksArray[i++] = hash;
                             }
                             this.sendBlocks(playerArray, blocksArray, UpdateBlockPacket.FLAG_ALL);
@@ -1955,7 +1981,7 @@ public class Level implements ChunkManager, Metadatable {
     public void updateBlockSkyLight(int x, int y, int z) {
     }
 
-    public void updateBlockLight(Map<Long, Map<Character, Object>> map) {
+    public void updateBlockLight(Map<Long, Map<Integer, Object>> map) {
         int size = map.size();
         if (size == 0) {
             return;
@@ -1965,17 +1991,17 @@ public class Level implements ChunkManager, Metadatable {
         Long2ObjectOpenHashMap<Object> visited = new Long2ObjectOpenHashMap<>();
         Long2ObjectOpenHashMap<Object> removalVisited = new Long2ObjectOpenHashMap<>();
 
-        Iterator<Map.Entry<Long, Map<Character, Object>>> iter = map.entrySet().iterator();
+        Iterator<Map.Entry<Long, Map<Integer, Object>>> iter = map.entrySet().iterator();
         while (iter.hasNext() && size-- > 0) {
-            Map.Entry<Long, Map<Character, Object>> entry = iter.next();
+            Map.Entry<Long, Map<Integer, Object>> entry = iter.next();
             iter.remove();
             long index = entry.getKey();
-            Map<Character, Object> blocks = entry.getValue();
+            Map<Integer, Object> blocks = entry.getValue();
             int chunkX = Level.getHashX(index);
             int chunkZ = Level.getHashZ(index);
             int bx = chunkX << 4;
             int bz = chunkZ << 4;
-            for (char blockHash : blocks.keySet()) {
+            for (int blockHash : blocks.keySet()) {
                 int hi = (byte) (blockHash >>> 8);
                 int lo = (byte) blockHash;
                 int y = lo & 0xFF;
@@ -2078,16 +2104,16 @@ public class Level implements ChunkManager, Metadatable {
         }
     }
 
-    private final Map<Long, Map<Character, Object>> lightQueue = new ConcurrentHashMap<>(8, 0.9f, 1);
+    private final Map<Long, Map<Integer, Object>> lightQueue = new ConcurrentHashMap<>(8, 0.9f, 1);
 
     public void addLightUpdate(int x, int y, int z) {
         long index = chunkHash(x >> 4, z >> 4);
-        Map<Character, Object> currentMap = lightQueue.get(index);
+        Map<Integer, Object> currentMap = lightQueue.get(index);
         if (currentMap == null) {
             currentMap = new ConcurrentHashMap<>(8, 0.9f, 1);
             this.lightQueue.put(index, currentMap);
         }
-        currentMap.put(Level.localBlockHash(x, y, z), changeBlocksPresent);
+        currentMap.put(Level.localBlockHash(x, y, z, this.getDimensionData()), changeBlocksPresent);
     }
 
     @Override
@@ -2193,13 +2219,13 @@ public class Level implements ChunkManager, Metadatable {
 
     private void addBlockChange(long index, int x, int y, int z) {
         synchronized (changedBlocks) {
-            SoftReference<Map<Character, Object>> current = changedBlocks.computeIfAbsent(index, k -> new SoftReference<>(new HashMap<>()));
-            Map<Character, Object> currentMap = current.get();
+            SoftReference<Map<Integer, Object>> current = changedBlocks.computeIfAbsent(index, k -> new SoftReference<>(new HashMap<>()));
+            Map<Integer, Object> currentMap = current.get();
             if (currentMap != changeBlocksFullMap && currentMap != null) {
                 if (currentMap.size() > MAX_BLOCK_CACHE) {
                     this.changedBlocks.put(index, new SoftReference<>(changeBlocksFullMap));
                 } else {
-                    currentMap.put(Level.localBlockHash(x, y, z), changeBlocksPresent);
+                    currentMap.put(Level.localBlockHash(x, y, z, this.getDimensionData()), changeBlocksPresent);
                 }
             }
         }

src/main/java/cn/nukkit/block/BlockLiquid.java
@@ -282,7 +282,7 @@ public abstract class BlockLiquid extends BlockTransparentMeta {
             }
             if (decay >= 0) {
                 int bottomY = (int) this.y - 1;
-                if (bottomY >= 0) {
+                if (bottomY >= this.level.getMinBlockY()) {
                     Block bottomBlock = this.level.getBlock((int) this.x, bottomY, (int) this.z);
                     this.flowIntoBlock(bottomBlock, decay | 0x08);
                     if (decay == 0 || !(usesWaterLogging() ? bottomBlock.canWaterloggingFlowInto() : bottomBlock.canBeFlowedInto())) {
@@ -358,7 +358,7 @@ public abstract class BlockLiquid extends BlockTransparentMeta {
             } else if (j == 3) {
                 ++z;
             }
-            long hash = Level.blockHash(x, y, z);
+            long hash = Level.blockHash(x, y, z, this.level.getDimensionData());
             if (!this.flowCostVisited.containsKey(hash)) {
                 Block blockSide = this.level.getBlock(x, y, z);
                 if (!this.canFlowInto(blockSide)) {
@@ -421,14 +421,14 @@ public abstract class BlockLiquid extends BlockTransparentMeta {
             }
             Block block = this.level.getBlock(x, y, z);
             if (!this.canFlowInto(block)) {
-                this.flowCostVisited.put(Level.blockHash(x, y, z), BLOCKED);
+                this.flowCostVisited.put(Level.blockHash(x, y, z, this.level.getDimensionData()), BLOCKED);
             } else if (usesWaterLogging()?
                     this.level.getBlock(x, y - 1, z).canWaterloggingFlowInto():
                     this.level.getBlock(x, y - 1, z).canBeFlowedInto()) {
-                this.flowCostVisited.put(Level.blockHash(x, y, z), CAN_FLOW_DOWN);
+                this.flowCostVisited.put(Level.blockHash(x, y, z, this.level.getDimensionData()), CAN_FLOW_DOWN);
                 flowCost[j] = maxCost = 0;
             } else if (maxCost > 0) {
-                this.flowCostVisited.put(Level.blockHash(x, y, z), CAN_FLOW);
+                this.flowCostVisited.put(Level.blockHash(x, y, z, this.level.getDimensionData()), CAN_FLOW);
                 flowCost[j] = this.calculateFlowCost(x, y, z, 1, maxCost, j ^ 0x01, j ^ 0x01);
                 maxCost = Math.min(maxCost, flowCost[j]);
             }

src/main/java/cn/nukkit/entity/item/EntityItem.java
@@ -361,7 +361,7 @@ public class EntityItem extends Entity {
 
         this.checkBlockCollision();
 
-        if (this.y <= -16 && this.isAlive()) {
+        if (this.y <= (level.getMinBlockY() - 16) && this.isAlive()) {
             this.attack(new EntityDamageEvent(this, DamageCause.VOID, 10));
             hasUpdate = true;
         }

src/main/java/cn/nukkit/entity/item/EntityVehicle.java
@@ -65,7 +65,7 @@ public abstract class EntityVehicle extends Entity implements EntityRideable, En
 
     @Override
     public boolean onUpdate(int currentTick) {
-        if (y < -16) {
+        if (y < (level.getMinBlockY() - 16)) {
             this.close();
         }
 

commit e584a625f4711b3471668d2bd270b91a3a0b7526

fix(level): resend whole chunk when the block-change sentinel is hit
changeBlocksFullMap is a sentinel that is never populated, so its size() is
always 0. The tick flush classified it with `size() > MAX_BLOCK_CACHE`, which
therefore never matched: a chunk receiving 1026+ block changes in a single tick
fell into the per-block branch with a zero-length array and sent nothing at all.
Every non-direct block update for that chunk in that tick was silently dropped,
leaving ghost blocks until the chunk was resent. Reachable without plugins via
/fill, which only warns above 32768 blocks and continues.

Regression from 8470a571b, which replaced CharacterHashMap (size() overridden to
Character.MAX_VALUE for exactly this check) with a plain Int2ObjectOpenHashMap
and dropped the override.

Detect the sentinel by identity, keeping the existing null and size conditions so
the 1025-change case behaves as before. The check moves into a named
shouldResendWholeChunk() so it can be covered by LevelBlockChangeFlushTest.

Co-Authored-By: Claude Opus 5 (1M context) <noreply@anthropic.com>

src/main/java/cn/nukkit/level/Level.java
@@ -325,7 +325,8 @@ public class Level implements ChunkManager, Metadatable {
     private final Long2ObjectOpenHashMap<SoftReference<Map<Integer, Object>>> changedBlocks = new Long2ObjectOpenHashMap<>();
     // Storing the vector is redundant
     private final Object changeBlocksPresent = new Object();
-    // Storing extra blocks past 512 is redundant
+    // 哨兵:标记区块变更过多,tick 末尾整块重发;它永不写入,因此不能用 size() 判定
+    // Sentinel marking "too many changes, resend the whole chunk"; never populated, so never test it via size()
     private final Int2ObjectOpenHashMap<Object> changeBlocksFullMap = new Int2ObjectOpenHashMap<>();
 
     private final BlockUpdateScheduler updateQueue;
@@ -1254,7 +1255,7 @@ public class Level implements ChunkManager, Metadatable {
                         Map<Integer, Object> blocks = entry.getValue().get();
                         int chunkX = Level.getHashX(index);
                         int chunkZ = Level.getHashZ(index);
-                        if (blocks == null || blocks.size() > MAX_BLOCK_CACHE) {
+                        if (this.shouldResendWholeChunk(blocks)) {
                             FullChunk chunk = this.getChunk(chunkX, chunkZ);
                             if (chunk != null) {
                                 for (Player p : this.getChunkPlayers(chunkX, chunkZ).values()) {
@@ -2457,6 +2458,16 @@ public class Level implements ChunkManager, Metadatable {
                 }
             }
         }
+    }
+
+    /**
+     * 判断区块本 tick 是否需整块重发:引用被回收、命中哨兵、或变更数超过缓存上限。
+     * <p>
+     * Whether the chunk must be resent as a whole this tick: reference collected, sentinel hit,
+     * or changes above the cache cap. The sentinel is never populated, so it cannot be detected by size.
+     */
+    private boolean shouldResendWholeChunk(Map<Integer, Object> blocks) {
+        return blocks == null || blocks == changeBlocksFullMap || blocks.size() > MAX_BLOCK_CACHE;
     }
 
     public void antiXrayOnBlockChange(@Nullable Player player, @NotNull Vector3 vector3, int type) {

src/test/java/cn/nukkit/level/LevelBlockChangeFlushTest.java
@@ -1,0 +1,109 @@
+package cn.nukkit.level;
+
+import cn.nukkit.MockServer;
+import it.unimi.dsi.fastutil.ints.Int2ObjectOpenHashMap;
+import it.unimi.dsi.fastutil.longs.Long2ObjectOpenHashMap;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import java.lang.ref.SoftReference;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 方块变更刷新判定测试:变更数超过缓存上限时必须整块重发,不能因哨兵 size() 恒为 0 而静默丢弃更新。
+ * <p>
+ * Block-change flush tests: an over-cap chunk must trigger a whole-chunk resend, and must not be
+ * silently dropped because the sentinel map always reports size 0.
+ *
+ * @author LT_Name
+ */
+public class LevelBlockChangeFlushTest {
+
+    private Level newLevel() throws Exception {
+        MockServer.init();
+
+        Level level = Mockito.mock(Level.class, Mockito.CALLS_REAL_METHODS);
+        // CALLS_REAL_METHODS 不执行构造器,手动初始化本测试路径依赖的字段
+        // CALLS_REAL_METHODS skips constructors; init the fields this test path depends on
+        setField(level, "changedBlocks", new Long2ObjectOpenHashMap<SoftReference<Map<Integer, Object>>>());
+        setField(level, "changeBlocksPresent", new Object());
+        setField(level, "changeBlocksFullMap", new Int2ObjectOpenHashMap<>());
+        setField(level, "dimensionData", DimensionEnum.OVERWORLD.getDimensionData());
+        return level;
+    }
+
+    private static void setField(Object target, String name, Object value) throws Exception {
+        Field field = Level.class.getDeclaredField(name);
+        field.setAccessible(true);
+        field.set(target, value);
+    }
+
+    private static Object getField(Object target, String name) throws Exception {
+        Field field = Level.class.getDeclaredField(name);
+        field.setAccessible(true);
+        return field.get(target);
+    }
+
+    private static boolean shouldResendWholeChunk(Level level, Map<Integer, Object> blocks) throws Exception {
+        Method method = Level.class.getDeclaredMethod("shouldResendWholeChunk", Map.class);
+        method.setAccessible(true);
+        return (boolean) method.invoke(level, blocks);
+    }
+
+    private static void addBlockChange(Level level, long index, int x, int y, int z) throws Exception {
+        Method method = Level.class.getDeclaredMethod("addBlockChange", long.class, int.class, int.class, int.class);
+        method.setAccessible(true);
+        method.invoke(level, index, x, y, z);
+    }
+
+    @SuppressWarnings("unchecked")
+    private static Map<Integer, Object> changedBlocksFor(Level level, long index) throws Exception {
+        Long2ObjectOpenHashMap<SoftReference<Map<Integer, Object>>> changed =
+                (Long2ObjectOpenHashMap<SoftReference<Map<Integer, Object>>>) getField(level, "changedBlocks");
+        return changed.get(index).get();
+    }
+
+    @SuppressWarnings("unchecked")
+    @Test
+    public void sentinelIsEmptySoSizeCannotDetectIt() throws Exception {
+        Level level = newLevel();
+        Map<Integer, Object> sentinel = (Map<Integer, Object>) getField(level, "changeBlocksFullMap");
+
+        Assertions.assertEquals(0, sentinel.size(), "sentinel is never populated");
+        Assertions.assertTrue(shouldResendWholeChunk(level, sentinel), "sentinel must be detected by identity, not size");
+    }
+
+    @Test
+    public void collectedReferenceResendsWholeChunk() throws Exception {
+        Assertions.assertTrue(shouldResendWholeChunk(newLevel(), null));
+    }
+
+    @Test
+    public void smallChangeSetSendsIndividualBlocks() throws Exception {
+        Level level = newLevel();
+        Map<Integer, Object> blocks = new HashMap<>();
+        for (int i = 0; i < Level.MAX_BLOCK_CACHE; i++) {
+            blocks.put(i, new Object());
+        }
+        Assertions.assertFalse(shouldResendWholeChunk(level, blocks));
+    }
+
+    @Test
+    public void overflowingChunkStillDeliversUpdates() throws Exception {
+        Level level = newLevel();
+        long index = Level.chunkHash(0, 0);
+        // 每个 i 对应唯一的 (x, z, y),确保 localBlockHash 不重复
+        // Each i maps to a unique (x, z, y) so localBlockHash values never collide
+        for (int i = 0; i <= Level.MAX_BLOCK_CACHE + 1; i++) {
+            addBlockChange(level, index, i & 15, i >> 8, (i >> 4) & 15);
+        }
+
+        Map<Integer, Object> blocks = changedBlocksFor(level, index);
+        Assertions.assertSame(getField(level, "changeBlocksFullMap"), blocks, "over-cap chunk must switch to the sentinel");
+        Assertions.assertTrue(shouldResendWholeChunk(level, blocks), "sentinel must resend the chunk, otherwise this tick's updates are lost");
+    }
+}