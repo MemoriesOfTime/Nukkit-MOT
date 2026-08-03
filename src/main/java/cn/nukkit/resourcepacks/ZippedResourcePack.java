ba34131bcebce7ec0cc4237d3e43e8ecb8decb3d
LT_Name <45508179+lt-name@users.noreply.github.com>
2026-08-03 21:36:08 +0800

    feat(resourcepack): add CDN configuration and migrate pack storage (#790)

--- src/main/java/cn/nukkit/resourcepacks/ZippedResourcePack.java
+++ src/main/java/cn/nukkit/resourcepacks/ZippedResourcePack.java
@@ -19,9 +19,6 @@ public class ZippedResourcePack extends AbstractResourcePack {
     private File file;
     private byte[] sha256;
 
-    private String encryptionKey = "";
-    private String cdnUrl = "";
-
     public ZippedResourcePack(File file) {
         this(file, SupportType.UNIVERSAL);
     }
@@ -72,10 +69,8 @@ public class ZippedResourcePack extends AbstractResourcePack {
             if (parentFolder == null || !parentFolder.isDirectory()) {
                 throw new IOException("Invalid resource pack path");
             }
-            File keyFile = new File(parentFolder, this.file.getName() + ".key");
-            if (keyFile.exists()) {
-                this.encryptionKey = Files.readString(keyFile.toPath());
-            }
+            // 加密密钥只能来自 packs.yml（由 ResourcePackManager.applyPackConfig 注入）。
+            // Encryption keys now come exclusively from packs.yml (injected by ResourcePackManager.applyPackConfig).
         } catch (IOException e) {
             Server.getInstance().getLogger().logException(e);
         }
@@ -123,17 +118,11 @@ public class ZippedResourcePack extends AbstractResourcePack {
         return chunk;
     }
 
-    @Override
-    public String getEncryptionKey() {
-        return this.encryptionKey;
-    }
-
-    @Override
-    public String getCDNUrl() {
-        return this.cdnUrl;
-    }
-
+    /**
+     * @deprecated Use {@link #setCDNUrl(String)} instead
+     */
+    @Deprecated
     public void setCdnUrl(String cdnUrl) {
-        this.cdnUrl = cdnUrl;
+        this.setCDNUrl(cdnUrl);
     }
 }
