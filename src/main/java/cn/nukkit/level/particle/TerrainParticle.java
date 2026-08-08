d00efcb2064961789ecbbbbbf64b2c19fb1683e5
LT_Name <572413378@qq.com>
2026-08-08 22:07:29 +0800

    fix(protocol): correct block-particle encoding and biome cache for hashed network IDs

--- src/main/java/cn/nukkit/level/particle/TerrainParticle.java
+++ src/main/java/cn/nukkit/level/particle/TerrainParticle.java
@@ -4,14 +4,39 @@ import cn.nukkit.GameVersion;
 import cn.nukkit.block.Block;
 import cn.nukkit.level.GlobalBlockPalette;
 import cn.nukkit.math.Vector3;
+import cn.nukkit.network.protocol.DataPacket;
+import cn.nukkit.network.protocol.LevelEventPacket;
 
 /**
  * Created on 2015/11/21 by xtypr.
  * Package cn.nukkit.level.particle in project Nukkit .
  */
-public class TerrainParticle extends GenericParticle {
+public class TerrainParticle extends Particle {
+
+    protected final int blockId;
+    protected final int blockDamage;
 
     public TerrainParticle(Vector3 pos, Block block) {
-        super(pos, Particle.TYPE_TERRAIN, GlobalBlockPalette.getOrCreateRuntimeId(GameVersion.getLastVersion(), block.getId(), block.getDamage()));
+        this(pos, block.getId(), block.getDamage());
+    }
+
+    public TerrainParticle(Vector3 pos, int blockId, int blockDamage) {
+        super(pos.x, pos.y, pos.z);
+        this.blockId = blockId;
+        this.blockDamage = blockDamage;
+    }
+
+    @Override
+    public DataPacket[] mvEncode(GameVersion protocol) {
+        LevelEventPacket packet = new LevelEventPacket();
+        packet.evid = (short) (LevelEventPacket.EVENT_ADD_PARTICLE_MASK | getMultiversionId(protocol, Particle.TYPE_TERRAIN));
+        packet.x = (float) this.x;
+        packet.y = (float) this.y;
+        packet.z = (float) this.z;
+        packet.data = GlobalBlockPalette.getOrCreateRuntimeId(protocol, blockId, blockDamage);
+        packet.protocol = protocol.getProtocol();
+        packet.gameVersion = protocol;
+        packet.tryEncode();
+        return new DataPacket[]{packet};
     }
 }
