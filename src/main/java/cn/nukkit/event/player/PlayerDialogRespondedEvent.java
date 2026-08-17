dccbe1f475a4b361bfed38b435ef73f1aac3b873
30780 <3078023566@qq.com>
2026-08-17 19:07:19 +0800

    Revert "   fix: PlayerDialogRespondedEvent NPE"
    This reverts commit 2a663a3cc36a8706696ece3030464c67776201b1.

--- src/main/java/cn/nukkit/event/player/PlayerDialogRespondedEvent.java
+++ src/main/java/cn/nukkit/event/player/PlayerDialogRespondedEvent.java
@@ -17,7 +17,6 @@ public class PlayerDialogRespondedEvent extends PlayerEvent {
     protected FormResponseDialog response;
 
     public PlayerDialogRespondedEvent(Player player, FormWindowDialog dialog, FormResponseDialog response) {
-        this.player = player;
         this.dialog = dialog;
         this.response = response;
     }
