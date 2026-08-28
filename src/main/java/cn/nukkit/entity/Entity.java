1d39e1215049343309b6b22cee0519752a941818
Kanelucky <tuankietdoan46@gmail.com>
2026-08-28 19:44:04 +0700

    feat: EntityFallEvent

--- src/main/java/cn/nukkit/entity/Entity.java
+++ src/main/java/cn/nukkit/entity/Entity.java
@@ -2556,6 +2556,12 @@ public abstract class Entity extends Location implements Metadatable {
                 Block down = this.level.getBlock(this.chunk, this.getFloorX(), this.getFloorY() - 1, this.getFloorZ(), 0, true);
                 int floor = down.getId();
 
+                EntityFallEvent event = new EntityFallEvent(this, down, fallDistance);
+                if (event.isCancelled()) {
+                    return;
+                }
+                fallDistance = event.getFallDistance();
+
                 if (!this.noFallDamage) {
                     float damage = (float) Math.floor(fallDistance - 3 - (this.hasEffect(Effect.JUMP) ? this.getEffect(Effect.JUMP).getAmplifier() + 1 : 0));
 
---
+++ src/main/java/cn/nukkit/event/entity/EntityFallEvent.java
@@ -0,0 +1,31 @@
+package cn.nukkit.event.entity;
+
+import cn.nukkit.block.Block;
+import cn.nukkit.entity.Entity;
+import cn.nukkit.event.Cancellable;
+
+/**
+ * Called when an entity falls.
+ */
+public class EntityFallEvent extends EntityEvent implements Cancellable {
+    private Block blockFallOn;
+    private float fallDistance;
+
+    public EntityFallEvent(Entity entity, Block blockFallOn, float fallDistance) {
+        this.entity = entity;
+        this.blockFallOn = blockFallOn;
+        this.fallDistance = fallDistance;
+    }
+
+    public Block getBlockFallOn() {
+        return blockFallOn;
+    }
+
+    public float getFallDistance() {
+        return fallDistance;
+    }
+
+    public void setFallDistance(float distance) {
+        this.fallDistance = distance;
+    }
+}
